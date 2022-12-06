79823154b991639f0631268d38c4e58cf68fc329
stephane71 <stephane@un-bourguignon.com>
2022-12-06 18:35:37 +0100

    Store | add covalentApi reducer

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import walletReducer from "./wallet/slice";
+import { covalentApi } from "../services";
 
 export const store = configureStore({
   reducer: {
+    [covalentApi.reducerPath]: covalentApi.reducer,
     wallet: walletReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(covalentApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
