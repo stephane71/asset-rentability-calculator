2c66120af9829125570f0ab0fa990c7461c675e1
stephane71 <stephane@un-bourguignon.com>
2022-12-06 18:54:52 +0100

    Home | implement useGetTransactionsQuery

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,32 @@ import { useSelector, useDispatch } from "react-redux";
 import { walletAddressSelector } from "../store/wallet/selectors";
 import { setAddress } from "../store/wallet/slice";
 import { useEffect } from "react";
+import {
+  useGetTransactionsQuery,
+  useGetTransactionsQueryType,
+} from "../services/covalent";
 
 export default function Home(): JSX.Element {
   const address = useSelector(walletAddressSelector);
   const dispatch = useDispatch();
+  const { data, error }: useGetTransactionsQueryType = useGetTransactionsQuery({
+    address: process.env.NEXT_PUBLIC_WALLET_ADDRESS || "",
+    network: 137,
+  });
 
   useEffect(() => {
     dispatch(setAddress(process.env.NEXT_PUBLIC_WALLET_ADDRESS || ""));
   }, [dispatch]);
 
+  if (error) {
+    return <div>Something wrong</div>;
+  }
+
   return (
     <div>
       <div>Home</div>
       <div>{address}</div>
+      {data ? data.items.length : []}
     </div>
   );
 }
