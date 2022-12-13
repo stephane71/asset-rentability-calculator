7d41133a6bcd32d0477c8803c024551f10f3fddb
stephane71 <stephane@un-bourguignon.com>
2022-12-13 18:10:45 +0100

    App | add handling msw service worker setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { store } from "../store/store";
+import { store } from "../store";
 import { Provider } from "react-redux";
+import { useEffect, useState } from "react";
+
+function prepare() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = require("../mocks/browser");
+    return worker.start();
+  }
+  return Promise.resolve();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loadingSW, setLoadingSW] = useState(true);
+
+  useEffect(() => {
+    prepare().then(() => setLoadingSW(false));
+  }, []);
+
+  if (loadingSW) {
+    return <></>;
+  }
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
