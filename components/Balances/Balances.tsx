1ab42c487217531070faa0930e3ed79f28e16b16
stephane71 <stephane@un-bourguignon.com>
2022-12-21 21:48:13 +0100

    Components | Balances: create component

---
+++ components/Balances/Balances.tsx
@@ -0,0 +1,29 @@
+import { BalanceItem } from "../../services/covalent/types";
+import { formatDecimals } from "../../utils";
+
+type Props = {
+  balances: BalanceItem[];
+};
+
+export function Balances({ balances }: Props) {
+  return (
+    <div style={{ padding: 24 }}>
+      {balances
+        .filter(({ balance }) => balance !== "0")
+        .map(
+          ({
+            contract_address,
+            contract_name,
+            contract_ticker_symbol,
+            balance,
+            contract_decimals,
+          }) => (
+            <div key={contract_address}>
+              {contract_name} - {contract_ticker_symbol} -{" "}
+              {formatDecimals(Number.parseInt(balance), contract_decimals)}
+            </div>
+          )
+        )}
+    </div>
+  );
+}
