e4f1c4fd66960f4d2ccffb3b26ad9751b522563c
stephane71 <stephane@un-bourguignon.com>
2023-02-14 16:14:39 +0100

    Components | TransactionListMetaData: format code

--- components/TransationListMetaData/TransationListMetaData.tsx
+++ components/TransationListMetaData/TransationListMetaData.tsx
@@ -7,7 +7,7 @@ type Props = {
 export function TransactionListMetaData({ transactions }: Props): JSX.Element {
   const { updated_at, quote_currency, pagination } = transactions;
   return (
-    <div  style={{ padding: 48 }}>
+    <div style={{ padding: 48 }}>
       <div>Last update {updated_at}</div>
       <div>Currency {quote_currency}</div>
       <div>Pagination {JSON.stringify(pagination)}</div>
